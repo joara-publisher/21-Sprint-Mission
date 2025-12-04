57b16fc6a34b341b8adf63f575d09593c75ac197
joara-thrlaw <arjoe207@gmail.com>
2025-12-04 15:11:02 +0900

    feat: redirect root path to /items

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './Layout';
 import ItemListPage from './Pages/ItemListPage';
 
 function Main() {
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/" element={<Navigate to="/items" replace />} />
+          
+          
           <Route path="/items" element={<ItemListPage />} />
         </Route>
       </Routes>
